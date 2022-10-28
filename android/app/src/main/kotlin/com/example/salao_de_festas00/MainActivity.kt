0a09b8c524de47dc9dddc15aae76b4fdf639ce58
avitu <andresoad13@gmail.com>
2022-10-28 14:04:44 -0300

    Start Commit!

---
+++ android/app/src/main/kotlin/com/example/salao_de_festas00/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.salao_de_festas00
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'screens/telainicial.dart';
+
+void main() {
+  runApp(const MyApp());
+}
